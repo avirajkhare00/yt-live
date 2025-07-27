7d5ca4bd5510a0048a1353932a2bf31b79946dee
avirajkhare00 <avirajkhare00@gmail.com>
2025-07-28 00:01:48 +0530

    Added stream endpoint and some html

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import morgan from "morgan";
+import path from "path";
 
 const PORT:string|number = process.env.PORT || 8000;
 
@@ -8,6 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.get('/stream', async (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'public', 'stream.html'));
+})
 
 app.get('/ping', async (req: Request, res: Response) => {
   res.json({ "status": "pong" });
